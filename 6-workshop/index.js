f300dce87159e136387c93cf6212faa9f72c93b7
dapraws <darrelprawira26@gmail.com>
2024-08-27 21:04:34 +0700

    Package Testing

---
+++ 6-workshop/index.js
@@ -0,0 +1,8 @@
+const perkenalan = require("perkenalan");
+
+const nama = "Darrel";
+const asal = "Jakarta";
+const hobi = "coding";
+
+const kenalkan = perkenalan(nama, asal, hobi);
+console.log(kenalkan);
---
+++ 6-workshop/node_modules/perkenalan/index.js
@@ -0,0 +1,5 @@
+function perkenalan(name, origin, hobby) {
+  return `Halo, namaku ${name} dari ${origin}, dan hobiku ${hobby}. Salam kenal!`;
+}
+
+module.exports = perkenalan;
